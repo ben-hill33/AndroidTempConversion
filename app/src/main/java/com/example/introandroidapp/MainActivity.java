6fd738b1eced980b1d5f9cb6f309d0d6d7cd813f
ben-hill33 <benjamin.hill28@gmail.com>
2022-11-10 15:38:07 -0800

    Added user input with events Added toasts to alert user based on conditions Updated interface to ten-key digits rather than keyboard.

--- app/src/main/java/com/example/introandroidapp/MainActivity.java
+++ app/src/main/java/com/example/introandroidapp/MainActivity.java
@@ -3,10 +3,26 @@ package com.example.introandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener, OnClickListener {
+
+    // Class variables
+    private RadioButton btnFah;
+    private RadioButton btnCel;
+    private RadioButton btnKel;
+    private EditText txtInputTemp;
+    private TextView lblOutputDegF;
+    private TextView lblOutputDegC;
+    private TextView lblOutputDegK;
+    private Button btnConvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +30,62 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         setContentView(R.layout.activity_main);
         RadioGroup tempGroup = (RadioGroup) findViewById(R.id.tempGroup);
         tempGroup.setOnCheckedChangeListener(this);
+        btnConvert = (Button) findViewById(R.id.btnConvert);
+        btnConvert.setOnClickListener(this);
+    }
+
+    private void displayToast( double temperature ) {
+
+        if(temperature > 50)
+            Toast.makeText(this, "Wow, it's hot outside!", Toast.LENGTH_LONG).show();
+        else if( temperature > 20)
+            Toast.makeText(this, "Nice weather we're having.", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "Brrr, it's cold out!", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //Get Resources for radio buttons, text boxes, and labels
+        btnFah = (RadioButton) findViewById(R.id.btnDegF);
+        btnCel = (RadioButton) findViewById(R.id.btnDegC);
+        btnKel = (RadioButton) findViewById(R.id.btnDegK);
+        txtInputTemp = (EditText) findViewById(R.id.txtInputTemp);
+        lblOutputDegF = (TextView) findViewById(R.id.lblOutputF);
+        lblOutputDegC = (TextView) findViewById(R.id.lblOutputC);
+        lblOutputDegK = (TextView) findViewById(R.id.lblOutputK);
+
+        //Convert the input temperature to double
+        double temp = Double.parseDouble(String.valueOf(txtInputTemp.getText()));
+        double answer = 0;
+
+        //If the user chooses Fahrenheit, convert to Celsius and Kelvin and display in those text boxes
+        if(btnFah.isChecked()) {
+            lblOutputDegF.setText(temp + " degrees F");
+            lblOutputDegC.setText((Math.round((temp - 32) * 5 / 9 * 100.0)/100.0) + " degrees C");
+            lblOutputDegK.setText((Math.round((temp + 459.67) * 5 / 9 * 100.0)/100.0) + " degrees K");
+            answer = (temp - 32) * 5/9;
+        }
+
+        //If the user chooses Celsius, convert to Fahrenheit and Kelvin and display in those text boxes
+        if(btnCel.isChecked()) {
+            lblOutputDegF.setText((Math.round(((temp * 9) / 5 + 32) * 100.0) / 100.0)+" degrees F");
+            lblOutputDegC.setText(temp + " degrees C");
+            lblOutputDegK.setText((Math.round((temp + 273.15) * 100.0) / 100.0) + " degrees K");
+            answer = temp;
+        }
+
+        //If the user chooses Kelvin, convert to Fahrenheit and Celsius and display in those text boxes
+        if(btnKel.isChecked()) {
+            lblOutputDegF.setText((Math.round((temp * 9 / 5 - 459.67) * 100.0) / 100.0) + " degrees F");
+            lblOutputDegC.setText((Math.round((temp - 273.15) * 100.0) / 100.0) + " degrees C");
+            lblOutputDegK.setText(temp + " degrees K");
+            answer = temp - 273.15;
+        }
+
+        displayToast(answer);
     }
 
     @Override
